9158b1e6bd1e8d0f297005ae3d30d06fd99bda12
arielm <arielm@e-bitbox.com>
2022-02-07 13:14:38 +0200

    loading newConfigMap for priority rules

---
+++ TypesHierarchy.py
@@ -0,0 +1,15 @@
+types_to_subtypes_relations = {
+    "AttackIndication": {
+        "BlackMarket",
+        "BotDataForSale",
+    },
+    "DataLeakage": {
+        "ConfidentialDocumentLeakage",
+        "ConfidentialInformationExposed",
+        "CredentialsLeakage",
+        "ExposedMentionsOnGithub",
+    },
+    "vip": {
+        "BlackMarket",
+    }
+}
--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from config.AlertProperties import AlertPriority
 from data.AlertExamples import alerts
+import SerachPatternAlgorithm
 
 import ciso8601
 import time
 import operator
-import SerachPatternAlgorithm
 
 configCherryPickNum = 4
 topPriorityAlerts = []
 priorityLevelsMatchingAlerts = [dict() for num in range(len(AlertPriority.templates))]
+newPriorityConfig = {}
 
 
 def get_alert_id(alert):
     return alert['_id']
+
+
+def get_alert_type_key(alert):
+    return alert['Details']['Type'] + "_" + alert['Details']['SubType']
 
 
 def get_alert_timestamp(alert):
@@ -37,23 +42,25 @@ def types_match(in_alert, rule):
 
 def find_match(in_alert):
     # Iterating the alert over priority map,
-    # And if titles match we send for types match and return a priority # on full match
-    for priority in AlertPriority.templates.items():
-        for rule in priority[1]:
-            for title in rule['title_identifiers']:
-                if SerachPatternAlgorithm.KMPSearch(title, in_alert['Title']) \
-                        and types_match(in_alert, rule):
-                    return priority[0]
+    # Calling for types match if the titles matched. then and return a priority # on full match
+    type_key_match = newPriorityConfig.get(get_alert_type_key(in_alert))
+    if not type_key_match:
+        return -1
+
+    for title in type_key_match.items():
+        if SerachPatternAlgorithm.KMPSearch(title[0], in_alert['Title']):
+            return title[1]  # priority number
+
     return -1
 
 
 def find_full_matches(incoming_alerts):
     """
     This function iterate over incoming alerts and match it against the priority config
-    1. We iterate each alert over the priority level config from top priority to lowest
-    2. First we try to find a matching title
-    3. If found then we compare subtype and type (if not found we continue with same alert to the next priority rule)
-    4. If a full match is found we insert the alert ID and timestamp into the matching alerts map.
+    1. An iteration is done each alert over the priority level config from top priority to lowest
+    2. First an attempt is made to find a matching type subtype
+    3. If found then a comparison of titles is made (if not found it continue with same alert to the next priority rule)
+    4. If a full match is found it insert the alert ID and timestamp into the matching alerts map.
     :param incoming_alerts:
     :return:
     """
@@ -80,7 +87,7 @@ def cherry_pick_top_alerts():
     """
     This function iterate over the list of matching alerts by priority,
     From the top priority, down to the lowest one.
-    For each priority we sort all the matching alerts by most recent timestamp
+    For each priority a sorting to all the matching alerts by most recent timestamp is done
     The end results will be the topPriorityAlerts list filled with the (configured) cherry-picked top priority alerts
     :return:
     """
@@ -97,11 +104,18 @@ def cherry_pick_top_alerts():
 
 def find_top_priority_alerts(incoming_alerts):
     """
-    Implement logic is as follows:
-    1. We receive the incoming alerts file along with priority configuration mapping
+    Implement logic algorithm is as follows:
+    1. first it takes the alerts priority rule map and prepare a new priority rule map called 'newPriorityConfig'
+        1.1. the newPriorityConfig is a dictionary where:
+            the key is: 'type_subtype'
+            the value is: a dictionary where
+                the key is: title
+                the value is: priority number
+            meaning it inserts all the titles and thier priority number,
+            that are connected to this type and subtype as key:value pairs
     2. find_full_matches()
-        2.1. first matches titles or contained titles
-        2.2. then matches type and subtype
+        2.1. first it finds a type_subtype key within the newPriorityConfig dictionary
+        2.2. then try to match any of its titles to the alert title (if found, the priority number is returned as value)
         2.3. each match will be inserted into the list.
         2.4. each element in the list represents a priority level and contains a dictionary,
         Of all the matched alerts with the same priority level.
@@ -120,5 +134,30 @@ def find_top_priority_alerts(incoming_alerts):
     cherry_pick_top_alerts()
 
 
+def load_priority_rule_map():
+    """
+    takes the alerts priority rule map and prepare a new priority rule map called 'newPriorityConfig'
+    the newPriorityConfig is a dictionary where:
+    the key is: 'type_subtype'
+    the value is: a dictionary where
+        the key is: title
+        the value is: priority number
+    meaning it inserts all the titles and thier priority number,
+    that are connected to this type and subtype as key:value pairs
+    :return:
+    """
+    config = AlertPriority.templates
+    for priorityValue in config.items():
+        for option in priorityValue[1]:
+            key = option.get('alert_type') + '_' + option.get('alert_subtype')
+            for title in option.get('title_identifiers'):
+                key_found = newPriorityConfig.get(key)
+                if key_found:
+                    key_found.update({title: priorityValue[0]})
+                else:
+                    newPriorityConfig.update({key: {title: priorityValue[0]}})
+
+
 if __name__ == '__main__':
+    load_priority_rule_map()
     find_top_priority_alerts(alerts)
